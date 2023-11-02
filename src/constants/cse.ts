7eb91048c2956452e814cdc5871d479d353311c1
Rohit Yadav <rk4740779@gmail.com>
2023-11-02 22:17:57 +0530

    added sem-3 content

--- src/constants/cse.ts
+++ src/constants/cse.ts
@@ -2,25 +2,25 @@ const csebtechsem1 = [
   {
     id: 1,
     subjectName: "End Term Exam",
-    contentType: "2022-2023",
+    contentType: "2023",
     driveLink: "https://drive.google.com/file/d/1XTKNSKZpyuAQqPtuGnRYWpDvIADpyM6N/view?usp=sharing"
   },
   {
     id: 2,
     subjectName: "End Term Exam",
-    contentType: "2021-2022",
+    contentType: "2022",
     driveLink: "https://drive.google.com/file/d/1yu71W92m8h6MsiwwjnfSPtX9s67h938r/view?usp=sharing"
   },
   {
     id: 3,
     subjectName: "End Term Exam",
-    contentType: "2018-2019",
+    contentType: "2019",
     driveLink: "https://drive.google.com/file/d/1v5Rt1-_Su0x7-q50V-rWak4UQkbaxBSo/view?usp=sharing"
   },
   {
     id: 4,
     subjectName: "Sessional",
-    contentType: "2019-2020",
+    contentType: "2020",
     driveLink: "https://drive.google.com/file/d/1jafPkN_IRMYTHBFFiL8QVAG5Q7-Qna5j/view?usp=sharing"
   },
   {
@@ -67,6 +67,85 @@ const csebtechsem1 = [
     driveLink: "https://drive.google.com/file/d/1H4iUxDsHP9_-nRGHc4kI5wOr403gu3X3/view?usp=sharing"
   },
 ]
+
+const csebtechsem2 = [
+  {
+    id: 1,
+    subjectName: "End Term Exam",
+    contentType: "2023",
+    driveLink: "https://drive.google.com/file/d/1AHbMlGuV7uUHHOrn9cghubdFu7bc4Za-/view?usp=sharing"
+  }, {
+    id: 2,
+    subjectName: "Sessional",
+    contentType: "1st Sessional",
+    driveLink: "https://drive.google.com/file/d/1sUTgKwjvUGM8bOB5uRmp49tob0qdHUmC/view?usp=sharing"
+  }, {
+    id: 3,
+    subjectName: "Sessional",
+    contentType: "2nd Sessional",
+    driveLink: "https://drive.google.com/file/d/1AvVgRx4ImyxPllxy-wVfyB2P40xGr2_K/view?usp=sharing"
+  }, {
+    id: 4,
+    subjectName: "COA Book Solution",
+    contentType: "M. MORRIS MANO Book Solution",
+    driveLink: "https://drive.google.com/file/d/11dY8En-zFzpiKpnJBPgRy4f3JF1ogBuk/view?usp=sharing"
+  }, {
+    id: 5,
+    subjectName: "COA Book",
+    contentType: "M. MORRIS MANO Book",
+    driveLink: "https://drive.google.com/file/d/1gEJ0IuoMHV32Yq9sf0OIFqCuzq5-Nec-/view?usp=sharing"
+  }, {
+    id: 6,
+    subjectName: "COA Notes",
+    contentType: "Unit 1",
+    driveLink: "https://drive.google.com/file/d/17-uccFmO8r92Vr65Q8wasoZ_P0Smqb73/view?usp=sharing"
+  }, {
+    id: 7,
+    subjectName: "COA Notes",
+    contentType: "Unit 2",
+    driveLink: "https://drive.google.com/file/d/1T80va-ZrnT5y0AOu-KAIx0nn2RzIH6KU/view?usp=sharing"
+  }, {
+    id: 8,
+    subjectName: "COA Notes",
+    contentType: "Unit 3",
+    driveLink: "https://drive.google.com/file/d/1ipfWqjOpBphQgaQKgTWY7sRNuREflInN/view?usp=sharing"
+  }, {
+    id: 9,
+    subjectName: "DSA Notes",
+    contentType: "Unit 1",
+    driveLink: "https://drive.google.com/file/d/1I0oQkHPn896x9BVdNs3dUS6DAVzPT5c4/view?usp=sharing"
+  }, {
+    id: 10,
+    subjectName: "DSA Notes",
+    contentType: "Unit 2",
+    driveLink: "https://drive.google.com/file/d/1GU0iBef4FIUbtiP3q-ilQeV6ZjUMIvSM/view?usp=sharing"
+  }, {
+    id: 11,
+    subjectName: "DSA Notes",
+    contentType: "Unit 4",
+    driveLink: "https://drive.google.com/file/d/1vlEn-U84liQTxd7eeErDOwC1ZZnDViwT/view?usp=sharing"
+  }, {
+    id: 12,
+    subjectName: "DE Notes",
+    contentType: "Part 1",
+    driveLink: "https://drive.google.com/file/d/1iQLUbbLiasNlsEg0E1XQ-5hrPHcpmLBA/view?usp=sharing"
+  }, {
+    id: 13,
+    subjectName: "DE Notes",
+    contentType: "Part 2",
+    driveLink: "https://drive.google.com/file/d/1xAiRdQmC0aqflZsEqRVD3zg3rI3UL_PO/view?usp=sharing"
+  }, {
+    id: 14,
+    subjectName: "DE Lab",
+    contentType: "Labfile",
+    driveLink: "https://drive.google.com/file/d/1gDdTPK-vvdJqknl8uqTR14kzmsBJ_9n9/view?usp=sharing"
+  },
+
+]
+
+
+
+
 
 // const csebtechsem2 = [
 //   {
@@ -123,4 +202,4 @@ const csebtechsem1 = [
 //   },
 // ]
 // export { csebtechsem1, csebtechsem2, csebtechsem3, csebtechsem4 };
-export { csebtechsem1 };
+export { csebtechsem1, csebtechsem2 };
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,13 @@
 // import { csebtechsem1, csebtechsem2, csebtechsem3, csebtechsem4 } from "./cse"
 // import { eesem1, eesem2, eesem3, eesem4 } from "./ee"
-import { csebtechsem1 } from "./cse"
+import { csebtechsem1, csebtechsem2 } from "./cse"
 
 interface Files {
     [key: string]: { subjectName: string; driveLink: string }[]; // Define the structure of the values
 }
 const files: Files = {
     // csebtechsem1, csebtechsem2, csebtechsem3, csebtechsem4, eesem1, eesem2, eesem3, eesem4
-    csebtechsem1
+    csebtechsem1, csebtechsem2
 }
 
 export default files;
